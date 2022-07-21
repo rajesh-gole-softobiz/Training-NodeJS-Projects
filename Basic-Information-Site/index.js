00a0086f3ff14de7c192c362a6e631a0df9b6d8f
rajesh-gol-softobiz <rajesh.gole@softobiz.com>
2022-07-22 00:14:41 +0530

    route created

--- Basic-Information-Site/index.js
+++ Basic-Information-Site/index.js
@@ -1,10 +1,37 @@
 const http = require('http');
-const port = 3000;
+const fs = require('fs');
+const port = 8080;
 const hostname= '127.0.0.1'
 
 const myServer = http.createServer((req,res)=>{
-    res.writeHead(202, {'Content-Type':'text/html'});
-    res.end('<h1>Welcome to Home Page...</h1>');
+    if(req.url === "/"){
+        fs.readFile('index.html',(err,data)=> {
+            res.writeHead(200, {"Content-Type":"text/html"});
+            res.write(data);
+            res.end();
+        })
+    }
+    else if(req.url === "/about"){
+        fs.readFile('about.html',(err,data)=> {
+            res.writeHead(200, {"Content-Type":"text/html"});
+            res.write(data);
+            res.end();
+        })
+    }
+    else if(req.url === "/contact"){
+        fs.readFile('contact-me.html',(err,data)=> {
+            res.writeHead(200, {"Content-Type":"text/html"});
+            res.write(data);
+            res.end();
+        })
+    }
+    else {
+            fs.readFile('404.html',(err,data)=> {
+            res.writeHead(404, {"Content-Type":"text/html"});
+            res.write(data);
+            res.end();
+        })
+    }
 });
 
 myServer.listen(port,hostname, ()=>{
