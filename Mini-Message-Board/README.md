3ff58a1d4f13b59479f3b3da7d4afb395c170d7d
rajesh-gol-softobiz <rajesh.gole@softobiz.com>
2022-07-24 18:55:45 +0530

    mini messageboard

---
+++ Mini-Message-Board/README.md
@@ -0,0 +1,9 @@
+# Mini message board
+
+### Steps to Run
+
+> npm install
+> npm start
+
+Visit to http://localhost:3000
+
---
+++ Mini-Message-Board/routes/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const router = express.Router();
+const distanceInWords = require("date-fns/formatDistanceToNow");
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Message Board", messages: messages, format: distanceInWords });
+});
+
+/* GET new message page. */
+router.get("/new", function (req, res, next) {
+  res.render("form");
+});
+
+router.post("/new", function (req, res) {
+  messages.push({
+    text: req.body.message,
+    user: req.body.author,
+    added: new Date(),
+  });
+  res.redirect("/");
+});
+
+module.exports = router;
